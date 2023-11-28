0219601be3774448a32330372c8d027e386075de
Tiana-Lewis <147452585+Tiana-Lewis@users.noreply.github.com>
2023-11-28 10:24:33 -0700

    button
    -button hover settings
    -counting feature (need to fix)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ let year = d.getUTCFullYear();
 document.getElementById("fyear").innerHTML = year;
 /* footer date */
 
-function myFunction() {
-    document.getElementById("like").innerHTML = "Thank you! :)";
+/*function myFunction() {
+    document.getElementById("txt-counter").innerHTML = "Thank you! :)";
   }
 /* buttons under photos */
+
+let count = 1;
+document.getElementById('btn-counter').onclick = () => {
+     count = count + 1;
+     document.getElementById('txt-counter').innerHTML = 'Likes: 1'
+}
+/*incrementing text*/
