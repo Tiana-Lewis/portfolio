c0a74393fc040756b192e128c23eedb1f265572d
Tiana-Lewis <147452585+Tiana-Lewis@users.noreply.github.com>
2023-11-28 10:39:46 -0700

    fixed counter
    -it works!!!!!! ahhhhh!!!!!!! used ' instead of ` by accident

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ let year = d.getUTCFullYear();
 document.getElementById("fyear").innerHTML = year;
 /* footer date */
 
-/*function myFunction() {
-    document.getElementById("txt-counter").innerHTML = "Thank you! :)";
-  }
-/* buttons under photos */
+
 
 let count = 1;
 document.getElementById('btn-counter').onclick = () => {
      count = count + 1;
-     document.getElementById('txt-counter').innerHTML = 'Likes: 1'
+     document.getElementById('txt-counter').innerHTML = `Likes: ${count}`;
 }
-/*incrementing text*/
+/*incrementing text and button*/
