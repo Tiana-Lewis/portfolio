2682acfdb96f17f0433da5b1e7cbb114a3fe6a3f
Tiana-Lewis <147452585+Tiana-Lewis@users.noreply.github.com>
2023-11-28 12:12:37 -0700

    alternating colors
    -trying to add alternating colors for numbers

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ let year = d.getUTCFullYear();
 document.getElementById("fyear").innerHTML = year;
 /* footer date */
 
-
+function myFunction() {
+    alert("Oh, hey.");
+  }
+/* alert */
 
 let count = 1;
 document.getElementById('btn-counter').onclick = () => {
@@ -11,3 +14,20 @@ document.getElementById('btn-counter').onclick = () => {
      document.getElementById('txt-counter').innerHTML = `Likes: ${count}`;
 }
 /*incrementing text and button*/
+
+document.getElementById('btn-alert').onmouseover = () => {
+    document.getElementById('btn-alert').innerText = 'Woah, wait-';
+};
+
+document.getElementById('btn-alert').onmouseleave = () => {
+    document.getElementById('btn-alert').innerText = 'What does this do?';
+};
+/*hover text change for button*/
+
+if (number % 2 == 0) {
+    document.getElementById('txt-counter').classList.add('even');
+} else {
+    document.getElementById('txt-counter').classList.add('odd');
+}
+
+/*even and off color change*/
