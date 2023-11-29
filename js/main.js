77b160d9538ab474aa9c62ef2f0a3b3adec72cf5
Tiana-Lewis <147452585+Tiana-Lewis@users.noreply.github.com>
2023-11-29 15:41:14 -0700

    added loop
    -added loop in html and javascript
    -used i++ instead of i = i + 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-/*document.addEventListener('DOMContentLoaded', function () {*/
-
+document.addEventListener('DOMContentLoaded', function () {
 const d = new Date();
 let year = d.getUTCFullYear();
 document.getElementById("fyear").innerHTML = year;
@@ -11,15 +10,21 @@ function myFunction() {
   }
 /* alert */
 
-// 
+document.getElementById('btn-alert').onmouseover = () => {
+    document.getElementById('btn-alert').innerText = 'Woah, wait-';
+};
 
-//function () {
+document.getElementById('btn-alert').onmouseleave = () => {
+    document.getElementById('btn-alert').innerText = 'What does this do?';
+};
+/*hover text change for button*/
+
     let count = 1;
 
     const outputElement = document.getElementById('txt-counter');
     const incrementButton = document.getElementById('btn-counter');
 
-    document.addEventListener('DOMContentLoaded'),function updateCounter() {
+    function updateCounter() {
         outputElement.innerHTML = `Likes: ${count}`;
 
         if (count % 2 === 0) {
@@ -37,17 +42,21 @@ function myFunction() {
     };
 
     updateCounter();
-//};
 
 /*even and odd color change*/
 
+const myList = document.getElementById('numbers');
 
+for (let i = 1; i <= 100; i++) { //i++ uses original value of i but increments instead of i = i + 1 which maintains value of i, does that explanation make sense? my brain hurts
+    const listItem = document.createElement('li');
+    listItem.textContent = `example ${i}`;
 
-document.getElementById('btn-alert').onmouseover = () => {
-    document.getElementById('btn-alert').innerText = 'Woah, wait-';
-};
+    const label = i % 2 === 0 ? 'Even' : 'Odd';
+        
+        listItem.textContent = `${label} - ${i}`;
 
-document.getElementById('btn-alert').onmouseleave = () => {
-    document.getElementById('btn-alert').innerText = 'What does this do?';
-};
-/*hover text change for button*/
+    myList.appendChild(listItem);
+}
+/*loop de loop*/
+
+});
