c19834751210ef06bc3386214c508cacf135531e
Tiana-Lewis <147452585+Tiana-Lewis@users.noreply.github.com>
2023-11-29 15:22:05 -0700

    Incrementing button and output
    -FINALLY FIXED THE COLOR ISSUE
    (css was overriding the changing colors)
    -and fixed up the JavaScript code for it

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+/*document.addEventListener('DOMContentLoaded', function () {*/
+
 const d = new Date();
 let year = d.getUTCFullYear();
 document.getElementById("fyear").innerHTML = year;
+
 /* footer date */
 
 function myFunction() {
@@ -8,12 +11,37 @@ function myFunction() {
   }
 /* alert */
 
-let count = 1;
-document.getElementById('btn-counter').onclick = () => {
-     count = count + 1;
-     document.getElementById('txt-counter').innerHTML = `Likes: ${count}`;
-}
-/*incrementing text and button*/
+// 
+
+//function () {
+    let count = 1;
+
+    const outputElement = document.getElementById('txt-counter');
+    const incrementButton = document.getElementById('btn-counter');
+
+    document.addEventListener('DOMContentLoaded'),function updateCounter() {
+        outputElement.innerHTML = `Likes: ${count}`;
+
+        if (count % 2 === 0) {
+            outputElement.classList.remove('odd');
+            outputElement.classList.add('even');
+        } else {
+            outputElement.classList.remove('even');
+            outputElement.classList.add('odd');
+        }
+    }
+
+    incrementButton.onclick = () => {
+        count++;
+        updateCounter();
+    };
+
+    updateCounter();
+//};
+
+/*even and odd color change*/
+
+
 
 document.getElementById('btn-alert').onmouseover = () => {
     document.getElementById('btn-alert').innerText = 'Woah, wait-';
@@ -23,11 +51,3 @@ document.getElementById('btn-alert').onmouseleave = () => {
     document.getElementById('btn-alert').innerText = 'What does this do?';
 };
 /*hover text change for button*/
-
-if (number % 2 == 0) {
-    document.getElementById('txt-counter').classList.add('even');
-} else {
-    document.getElementById('txt-counter').classList.add('odd');
-}
-
-/*even and off color change*/
